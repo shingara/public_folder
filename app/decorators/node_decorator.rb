e23c36d227eefc27484ca152a51d67696c87f242
Cyril Mougel <cyril.mougel@gmail.com>
2013-04-02 23:03:29 +0200

    improve view of content

--- app/decorators/node_decorator.rb
+++ app/decorators/node_decorator.rb
@@ -1,8 +1,13 @@
 class NodeDecorator < Draper::Decorator
   decorates :node
+  delegate :file_type
 
   def name
     h.link_to model.name, url
+  end
+
+  def size
+    h.number_to_human_size(model.size)
   end
 
   def directory_decorate
