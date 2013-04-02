1c68e9f8cfc5bef32c8b074da180dc745fbd0fe5
Cyril Mougel <cyril.mougel@gmail.com>
2013-04-02 22:56:25 +0200

    add COVERAGE env to avoid see all the time coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
-require 'coveralls'
-Coveralls.wear!('rails')
+
+if ENV['COVERAGE']
+  require 'coveralls'
+  Coveralls.wear!('rails')
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+end
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
